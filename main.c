71d8e5d7ac9c076f4ad31975151bdf367970c279
Amayas29 <amayas29sd@gmail.com>
2021-06-05 13:32:49 +0200

    Switch to continue git status

--- main.c
+++ main.c
@@ -1,15 +1,40 @@
 #include <ncurses.h>
 
+#ifdef WIN32
+#include <windows.h>
+#elif _POSIX_C_SOURCE >= 199309L
+#include <time.h>
+#else
+#include <unistd.h>
+#endif
+
+void sleep_ms(int milliseconds) {
+#ifdef WIN32
+    Sleep(milliseconds);
+#elif _POSIX_C_SOURCE >= 199309L
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+#else
+    if (milliseconds >= 1000)
+        sleep(milliseconds / 1000);
+    usleep((milliseconds % 1000) * 1000);
+#endif
+}
+
 #include "include/game.h"
 
 int main() {
-    enum Direction dir;
+    enum Direction dir = RIGHT;
 
-    Game *game = init_game(10);
+    Game *game = init_game(20);
 
     if (!game) return 1;
 
-    initscr();
+    WINDOW *w = initscr();
+    cbreak();
+    nodelay(w, TRUE);
 
     if (!has_colors()) {
         printw("\n\t Terminal does not support colors !");
@@ -30,7 +55,6 @@ int main() {
 
     game->map[1][8] = AppleCell;
 
-
     game->map[6][6] = AppleCell;
     game->map[5][8] = AppleCell;
     game->map[6][5] = AppleCell;
@@ -41,8 +65,6 @@ int main() {
         clear();
 
         show(game);
-
-        dir = None;
 
         key = getch();
 
@@ -61,6 +83,8 @@ int main() {
         if (!update(game, dir)) break;
 
         refresh();
+
+        sleep_ms(250);
     }
 
     printw("\n\tGame Over ...");
